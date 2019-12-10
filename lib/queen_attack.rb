e85976dde63d7a7d3d0de88542d2ba13eebfd716
Student <example@example.com>
2019-12-09 16:09:55 -0800

    fix horizontal and vertical

--- lib/queen_attack.rb
+++ lib/queen_attack.rb
@@ -1,8 +1,8 @@
 class Array
   def queen_attack?(queen_coords)
-    if self[0].abs === queen_coords[0].abs
+    if self[0] === queen_coords[0]
       return true
-    elsif self[1].abs === queen_coords[1].abs
+    elsif self[1] === queen_coords[1]
       return true
     elsif ((self[0] - queen_coords[0]).abs - (self[1] - queen_coords[1]).abs) === 0
       return true
